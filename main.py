11b828b8dcf50d6e74f0ef947413bf52bf4d1637
Ezylol <oliveira2509enzo@gmail.com>
2024-08-24 13:36:24 +0000

    massa

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-d = {"a": 1, "b": 2, "c": 3}}
-print(d.keys())
-print(d.values())
-print(d.items())
+print("DDD List: 61, 71, 11, 21, 32, 19, 27, 31")
+ddd = int(input())
+if ddd == 61:
+  print("Brasilia")
+elif ddd == 71:
+  print("Salvador")
+elif ddd == 11:
+  print("Sao Paulo")
+elif ddd == 21:
+  print("Rio de Janeiro")
+elif ddd == 32:
+  print("Juiz de Fora")
+elif ddd == 19:
+  print("Campinas")
+elif ddd == 27:
+  print("Vitoria")
+elif ddd == 31:
+  print("Belo Horizonte")
+else:
+  print("DDD nao cadastrado")
